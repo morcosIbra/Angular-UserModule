a448041709cdbee34d06b624327b4e434299801c
MorcosDan <mdanielibra@gmail.com>
2018-12-19 22:09:00 +0200

    usersList,signin,CRUD user  commit

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UsersService } from './users.service';
 import { Subscription } from 'rxjs';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-users',
@@ -10,7 +11,15 @@ import { Subscription } from 'rxjs';
 export class UsersComponent implements OnInit {
   subscription: Subscription;
   response: {};
-  constructor(private userService: UsersService) { }
+  showUserCard = false;
+  constructor(private userService: UsersService, private router: Router) { 
+    this.router.events.subscribe(e => {
+      if (e instanceof NavigationEnd) {
+        console.log(e.url, e.url.includes("/users/"))
+        this.showUserCard = e.url.includes("/users/")
+      }
+    });
+  }
 
   ngOnInit() {
     this.subscription = this.userService.actionStatus().subscribe(response => {
